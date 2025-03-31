5356eb4fb2025ef227371e37e74334fb03cd2752
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-03-31 16:14:39 -0600

    weather start

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # wdd230
  Web Frontend Development I
+
+514fcabd81719b73521e0fd6cff2f824
---
+++ week05/scripts/weather.js
@@ -0,0 +1,5 @@
+const currentTempo = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+const url = "//api.openweathermap.org/data/2.5/weather?";
