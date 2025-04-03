dd0a6f56b0b5b48bd5b125219836f0a85e35e3c1
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-04-03 10:21:24 -0600

    directory 4

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,35 +1,46 @@
+let membersData = [];
 const companiesURL = "./data/members.json";
+const membersContainer = document.querySelector("#members");
+const gridButton = document.querySelector(".grid-toggle");
+const tableButton = document.querySelector(".table-toggle");
 
 async function getCompanies() {
     try {
         const response = await fetch(companiesURL);
         if (!response.ok) throw new Error("Couldnt find data");
         const data = await response.json();
-        displayCompanies(data.members);
+        membersData = data.members;
+        displayCompaniesGrid();
         console.log("Se hizo correctamente");
     } catch (error) {
         console.error("Error:", error);
     }
 }
 
-function displayCompanies(members) {
-    const membersContainer = document.querySelector("#members");
+function clearMembersContainer() {
+    membersContainer.innerHTML = "";
+}
 
-    members.forEach(member => {
+function displayCompaniesGrid(members) {
+    clearMembersContainer();
+    membersContainer.classList.add("grid");
+    membersContainer.classList.remove("table");
+    
+    membersData.forEach(member => {
         let divMember = document.createElement("div");
         divMember.setAttribute("class", "member");
         let img = document.createElement("img");
         let address = document.createElement("p");
         let phone = document.createElement("p");
         let link = document.createElement("a");
-
+        
         img.setAttribute("src", member.image);
         img.setAttribute("alt", member.name);
         address.textContent = member.address;
         phone.textContent = member.phone;
         link.setAttribute("href", member.websiteURL);
         link.textContent = "Website";
-
+        
         divMember.appendChild(img);
         divMember.appendChild(address);
         divMember.appendChild(phone);
@@ -37,5 +48,42 @@ function displayCompanies(members) {
         membersContainer.appendChild(divMember);
     });
 }
+
+
+function displayCompaniesTable(members) {
+    clearMembersContainer();
+    membersContainer.classList.add("table");
+    membersContainer.classList.remove("grid");
+    
+    let table = document.createElement("table");
+    let body = document.createElement("tbody");
+    table.appendChild(body);
+    membersData.forEach(member => {
+        let row = document.createElement("tr");
+        let nameCell = document.createElement("td");
+        let addressCell = document.createElement("td");
+        let phoneCell = document.createElement("td");
+        let linkCell = document.createElement("td");
+        let websiteURL = document.createElement("a");
+        websiteURL.setAttribute("href", member.websiteURL);
+        websiteURL.textContent = "Website";
+        linkCell.appendChild(websiteURL);
+        
+        nameCell.textContent = member.name;
+        addressCell.textContent = member.address;
+        phoneCell.textContent = member.phone;
+        
+        row.appendChild(nameCell);
+        row.appendChild(addressCell);
+        row.appendChild(phoneCell);
+        row.appendChild(linkCell);
+        table.appendChild(row);
+        membersContainer.appendChild(table);
+    });
+}
+
+
+gridButton.addEventListener("click", displayCompaniesGrid);
+tableButton.addEventListener("click", displayCompaniesTable); 
 
 getCompanies();
